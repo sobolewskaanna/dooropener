fc67c0b8e21e3ed7afc2f7097cec7d0c0755d123
Anna Sobolewska <sobolewska.anna@gmail.com>
2015-03-03 21:09:05 -0800

    Twilio quickstart hello monkey

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'sinatra'
+gem 'twilio-ruby'
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
-  "Hello World!"
+  'Hello World! Currently running version ' + Twilio::VERSION + \
+        ' of the twilio-ruby library.'
 end
 
 get '/dooropener' do
   "hello"
+end
+
+get '/hello-monkey' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello Monkey'
+  end.text
 end
